61b000fcc59f6cf247e30156e8fbc88c829b9d41
Siounath <82611145+Esiounath@users.noreply.github.com>
2022-11-29 17:03:29 +0100

    New

--- Home/Home.js
+++ Home/Home.js
@@ -13,37 +13,59 @@ export default function Sidebar({navigation}){
   const dispatch = useDispatch()
   const userToken = userData.token ;
   const [ErrMsg,setErrMsg] = useState(false)
-  const [Alerts,setAlerts] = useState("")
+  const [Data,setData] = useState({})
   //console.log(userToken)
   function Test(){
-    return( <View>
-      <Text>Merci eden</Text>
-    </View>);
-  }
-  async function Events(){
-    await axios('/events',{
-      method:'GET',
-      headers:{
-        'content-type': 'application/json',
-      },
-      data:{
-        userToken,
-      },
-    }).then((reponse)=>{
-      setAlerts(JSON.stringify(reponse.data.events[4]))
-      console.log(typeof Alerts.event_type.map.name)
-      
-    }).catch((error)=>{
-      console.log("Erreur",error)
-    });
-  }
-  Events()
- useEffect(()=>{
-    if(userData.password === null && userData.token === null && userData.username === null && userData.alertNumber === null){
-      navigation.navigate('Login')
+    try{    
+      Events()
+      console.log(Data)
+      if(Data !== null || Data !== undefined){
+          return( 
+            <View style={{
+              flexDirection:'column',
+              justifyContent:'center',
+              textAlign:'center',
+              alignSelf:'center',
+              alignItems:'center',
+              marginTop:50,
+            }}>
+            <View style={styles.eventAlert}>
+              <Svg x="0px" y="0px"
+width="40" height="40"
+viewBox="0 0 50 50">
+                <Path d="M24 44q-4.15 0-7.8-1.575-3.65-1.575-6.35-4.275-2.7-2.7-4.275-6.35Q4 28.15 4 24t1.575-7.8Q7.15 12.55 9.85 9.85q2.7-2.7 6.35-4.275Q19.85 4 24 4t7.8 1.575q3.65 1.575 6.35 4.275 2.7 2.7 4.275 6.35Q44 19.85 44 24t-1.575 7.8q-1.575 3.65-4.275 6.35-2.7 2.7-6.35 4.275Q28.15 44 24 44Zm-2.15-3.05v-4.1q-1.75 0-2.95-1.3-1.2-1.3-1.2-3.05v-2.2L7.45 20.05q-.25 1-.35 1.975Q7 23 7 24q0 6.5 4.225 11.35t10.625 5.6Zm14.7-5.4q1.1-1.2 1.925-2.55.825-1.35 1.4-2.825t.85-3.025Q41 25.6 41 24q0-5.3-2.9-9.625T30.35 8.05v.9q0 1.75-1.2 3.05-1.2 1.3-2.95 1.3h-4.35v4.35q0 .85-.675 1.4-.675.55-1.525.55H15.5V24h12.9q.85 0 1.4.65.55.65.55 1.5v6.35h2.15q1.45 0 2.55.85 1.1.85 1.5 2.2Z" stroke="#7f8c8d" strokeWidth="1" width="30" height="30" fill="#7f8c8d"/>
+              </Svg>
+      <Text style={styles.textAlert}>{`Alertes : ${Data !== undefined ? Data.event_type.map.name : null}`}</Text>
+      </View>
+      </View>
+    );
+    }else{
+      return ;
     }
-  },[userData]);
-
+  }catch(error){
+    console.log(error)
+    }
+  }
+useEffect(()=>{
+  if(userData.password === null && userData.token === null && userData.username === null && userData.alertNumber === null){
+    navigation.navigate('Login')
+  }
+},[userData.username,userData.password,userData.token,userData.alertNumber]);
+async function Events(){
+  await axios('/events',{
+    method:'GET',
+    headers:{
+      'content-type': 'application/json',
+    },
+    data:{
+      userToken,
+    },
+  }).then((reponse)=>{
+    setData(reponse.data.events[4])
+  }).catch((error)=>{
+    console.log("Erreur",error)
+  });
+}
   const drawer = useRef(null);
 const drawerPosition = "left";
   const navigationView = () => (
@@ -64,6 +86,7 @@ viewBox="0 0 50 50">
         <Text style={styles.logout}>LogOut</Text>
         </Pressable>
       </View>
+      {Data !== undefined ? Test() : null}
     </SafeAreaView>
   );
 
@@ -90,6 +113,23 @@ viewBox="0 0 30 30" style={styles.menu}>
 };
 
 const styles = StyleSheet.create({
+  eventAlert:{
+    flex:1,
+    flexDirection:'row',
+    textAlign:'center',
+    alignItems:'flex-start',
+    justifyContent:'center',
+    alignSelf:'flex-start',
+  },
+  textAlert:{
+    color:'black',
+    fontSize:15,
+    fontStyle:'italic',
+    fontWeight:'500',
+    textTransform:'capitalize',
+    textAlign:'center',
+    margin:20,
+  },
   logout:{
     color:'#000',
     fontSize:22,
@@ -100,7 +140,8 @@ const styles = StyleSheet.create({
   },
   event:{
     flex:1,
-    flexDirection:'row',
+    flexDirection:'column',
+    textAlign:'center',
     alignItems:'flex-start',
     justifyContent:'flex-start',
     alignSelf:'flex-start',
