afccaf8ac4ba0d3f09a64cd1825e972e8f258e6a
Siounath <82611145+Esiounath@users.noreply.github.com>
2022-11-30 12:57:27 +0100

    New

--- Home/Home.js
+++ Home/Home.js
@@ -6,6 +6,7 @@ import {useSelector} from 'react-redux';
 import {useDispatch} from 'react-redux';
 import {logOut} from "../Redux/Slice";
 import axios from '../API/Argos_GET_USERS'
+import { Value } from "react-native-reanimated";
 
 
 export default function Sidebar({navigation}){
@@ -15,30 +16,42 @@ export default function Sidebar({navigation}){
   const [ErrMsg,setErrMsg] = useState(false)
   const [Data,setData] = useState({})
   //console.log(userToken)
-  function Test(){
+  function HomeAlert(){
+    try{
+      Events()
+      if(Data !== null || Data !== undefined){
+        return(<View style={styles.HomeAlert}>
+          <Image />
+          <Text style={styles.HomeText}></Text>
+          <Pressable>
+            <Image />
+          </Pressable>
+        </View>);
+      }
+    }catch(error){
+      console.log(error)
+    }
+  }
+  function Onglets(){
     try{    
       Events()
-      console.log(Data)
+      //console.log(Data)
       if(Data !== null || Data !== undefined){
+       const prop = Object.keys(Data.event_type.map).
+       filter((key) => key.includes('name')).
+       reduce((cur, key) => { return Object.assign(cur, { [key]: Data.event_type.map[key] })}, {});
+       for(const size in Object.keys(prop)){
           return( 
-            <View style={{
-              flexDirection:'column',
-              justifyContent:'center',
-              textAlign:'center',
-              alignSelf:'center',
-              alignItems:'center',
-              marginTop:50,
-            }}>
             <View style={styles.eventAlert}>
               <Svg x="0px" y="0px"
 width="40" height="40"
 viewBox="0 0 50 50">
                 <Path d="M24 44q-4.15 0-7.8-1.575-3.65-1.575-6.35-4.275-2.7-2.7-4.275-6.35Q4 28.15 4 24t1.575-7.8Q7.15 12.55 9.85 9.85q2.7-2.7 6.35-4.275Q19.85 4 24 4t7.8 1.575q3.65 1.575 6.35 4.275 2.7 2.7 4.275 6.35Q44 19.85 44 24t-1.575 7.8q-1.575 3.65-4.275 6.35-2.7 2.7-6.35 4.275Q28.15 44 24 44Zm-2.15-3.05v-4.1q-1.75 0-2.95-1.3-1.2-1.3-1.2-3.05v-2.2L7.45 20.05q-.25 1-.35 1.975Q7 23 7 24q0 6.5 4.225 11.35t10.625 5.6Zm14.7-5.4q1.1-1.2 1.925-2.55.825-1.35 1.4-2.825t.85-3.025Q41 25.6 41 24q0-5.3-2.9-9.625T30.35 8.05v.9q0 1.75-1.2 3.05-1.2 1.3-2.95 1.3h-4.35v4.35q0 .85-.675 1.4-.675.55-1.525.55H15.5V24h12.9q.85 0 1.4.65.55.65.55 1.5v6.35h2.15q1.45 0 2.55.85 1.1.85 1.5 2.2Z" stroke="#7f8c8d" strokeWidth="1" width="30" height="30" fill="#7f8c8d"/>
               </Svg>
-      <Text style={styles.textAlert}>{`Alertes : ${Data !== undefined ? Data.event_type.map.name : null}`}</Text>
-      </View>
+      <Text style={styles.textAlert}>{`${Data !== undefined ? prop.name : null}`}</Text>
       </View>
     );
+         }
     }else{
       return ;
     }
@@ -79,14 +92,14 @@ const drawerPosition = "left";
       <View style={styles.event}>
         <Pressable onPress={()=>{dispatch(logOut())}} style={styles.event}>
         <Svg x="0px" y="0px"
-width="40" height="40"
+width="30" height="30"
 viewBox="0 0 50 50">
-          <Path  d="m19.4 44-1-6.3q-.95-.35-2-.95t-1.85-1.25l-5.9 2.7L4 30l5.4-3.95q-.1-.45-.125-1.025Q9.25 24.45 9.25 24q0-.45.025-1.025T9.4 21.95L4 18l4.65-8.2 5.9 2.7q.8-.65 1.85-1.25t2-.9l1-6.35h9.2l1 6.3q.95.35 2.025.925Q32.7 11.8 33.45 12.5l5.9-2.7L44 18l-5.4 3.85q.1.5.125 1.075.025.575.025 1.075t-.025 1.05q-.025.55-.125 1.05L44 30l-4.65 8.2-5.9-2.7q-.8.65-1.825 1.275-1.025.625-2.025.925l-1 6.3ZM24 30.5q2.7 0 4.6-1.9 1.9-1.9 1.9-4.6 0-2.7-1.9-4.6-1.9-1.9-4.6-1.9-2.7 0-4.6 1.9-1.9 1.9-1.9 4.6 0 2.7 1.9 4.6 1.9 1.9 4.6 1.9Zm0-3q-1.45 0-2.475-1.025Q20.5 25.45 20.5 24q0-1.45 1.025-2.475Q22.55 20.5 24 20.5q1.45 0 2.475 1.025Q27.5 22.55 27.5 24q0 1.45-1.025 2.475Q25.45 27.5 24 27.5Zm0-3.5Zm-2.2 17h4.4l.7-5.6q1.65-.4 3.125-1.25T32.7 32.1l5.3 2.3 2-3.6-4.7-3.45q.2-.85.325-1.675.125-.825.125-1.675 0-.85-.1-1.675-.1-.825-.35-1.675L40 17.2l-2-3.6-5.3 2.3q-1.15-1.3-2.6-2.175-1.45-.875-3.2-1.125L26.2 7h-4.4l-.7 5.6q-1.7.35-3.175 1.2-1.475.85-2.625 2.1L10 13.6l-2 3.6 4.7 3.45q-.2.85-.325 1.675-.125.825-.125 1.675 0 .85.125 1.675.125.825.325 1.675L8 30.8l2 3.6 5.3-2.3q1.2 1.2 2.675 2.05Q19.45 35 21.1 35.4Z" stroke="#000" strokeWidth="1" width="40" height="40" fill="#000"/>
+          <Path  d="M22.5 26.1V5.8h3v20.3Zm1.5 16q-3.7 0-6.975-1.425Q13.75 39.25 11.3 36.8q-2.45-2.45-3.875-5.725Q6 27.8 6 24.1q0-4 1.7-7.475 1.7-3.475 4.8-6.175l2.1 2.1q-2.65 2.15-4.125 5.125T9 24.1q0 6.25 4.375 10.625T24 39.1q6.25 0 10.625-4.375T39 24.1q0-3.45-1.475-6.475Q36.05 14.6 33.5 12.55l2.15-2.1q3 2.55 4.675 6.1Q42 20.1 42 24.1q0 3.7-1.425 6.975-1.425 3.275-3.85 5.725-2.425 2.45-5.7 3.875Q27.75 42.1 24 42.1Z" stroke="#000" strokeWidth="1" width="40" height="40" fill="#000"/>
         </Svg>
         <Text style={styles.logout}>LogOut</Text>
         </Pressable>
       </View>
-      {Data !== undefined ? Test() : null}
+      {Data !== undefined ? Onglets() : null}
     </SafeAreaView>
   );
 
@@ -114,25 +127,25 @@ viewBox="0 0 30 30" style={styles.menu}>
 
 const styles = StyleSheet.create({
   eventAlert:{
-    flex:1,
+    flex:4,
     flexDirection:'row',
     textAlign:'center',
     alignItems:'flex-start',
-    justifyContent:'center',
+    justifyContent:'flex-start',
     alignSelf:'flex-start',
   },
   textAlert:{
     color:'black',
-    fontSize:15,
+    fontSize:18,
     fontStyle:'italic',
-    fontWeight:'500',
+    fontWeight:'700',
     textTransform:'capitalize',
     textAlign:'center',
-    margin:20,
+    margin:10,
   },
   logout:{
     color:'#000',
-    fontSize:22,
+    fontSize:20,
     fontStyle:'italic',
     fontWeight:'700',
     textTransform:'capitalize',
@@ -140,7 +153,7 @@ const styles = StyleSheet.create({
   },
   event:{
     flex:1,
-    flexDirection:'column',
+    flexDirection:'row',
     textAlign:'center',
     alignItems:'flex-start',
     justifyContent:'flex-start',
